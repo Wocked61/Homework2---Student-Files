76249d243841e05f1453113d4611e51250eab570
Wocked61 <DylanP@csu.fullerton.edu>
2025-03-13 20:02:25 -0700

    major breakthrough in the javascript which now displays the movies

--- Homework2 - Student Files/my-project/script.js
+++ Homework2 - Student Files/my-project/script.js
@@ -6,40 +6,46 @@ const options = {
   }
 };
 
-fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
+fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
   .then(res => res.json())
-  .then(data => console.log(data))
-  .then(res => console.log(res))
+  .then(data => {console.log(data)
+    showMovies(data)
+  })
   .catch(err => console.error(err));
 
-// const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTgyMTA5YjM5MjNkZmM2NjdmN2M4NDc5MDJlYWRhYSIsIm5iZiI6MTc0MTM5OTI5OC44NDE5OTk4LCJzdWIiOiI2N2NiYTUwMjlhM2Y3MDEzZmE0ZjY1ZWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.dCtlhuJ-_2oy8hXCgIbSgtN0WqogwdFrqCXg8a6gmQA'
-// const BASE_URL = 'https://api.moviedb.org/3';
-// const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+const main = document.getElementById('main');
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+function showMovies(data) {
+
+  main.innerHTML = '';
+
+  data.results.forEach(mov => {
+
+    
 
 
+    const{title,poster_path,release_date,vote_average} = mov
+    const movEL = document.createElement('div');
+    movEL.classList.add('movie');
+    movEL.innerHTML = `
+    <div class="box">
+      <img class="Picture" src="${IMG_URL+ poster_path}" alt="${title}" id="Movie_Picture">
+      <div>
+          <p class="pictureText" id="Movie_Title">${title}</p>
+          <p class="pictureDesc" id="Movie_Release">${"Release Date: "+release_date}</p>
+          <p class="pictureDesc" id="Movie_Rating">${"Rating: "+vote_average}</p>
+      </div>
+    </div>`
+  
 
-// const prev = document.getElementById('prev');
-// const next = document.getElementById('next');
-// const current = document.getElementById('current');
+    main.appendChild(movEL);
 
-// var currentPage = 1;
-// var nextPage = 2;
-// var prevPAge = 3;
-// var lastUrl = '';
-// var totalPages = 100;
+  });
 
-// //search bar functionality
-// const form = document.getElementById('form');
-// const search = document.getElementById('search');
+  const movie_search = document.getElementById("Movie_id").value.toLowerCase();
+  const page = data.page;
+  const total_pages = data.total_pages;
+ 
+}
 
-// form.addEventListener('submit' , (e)=> {
-//   e.preventDefault();
-
-//   const searchTerm = search.value;
-
-//   if(searchTerm){
-//     getMovies(searchURL+'&query=' + searchTerm);
-//   } else {
-//     getMovies(API_URL);
-//   }
-// })
