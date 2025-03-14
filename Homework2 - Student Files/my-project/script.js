4ad52b03c907edb33f3772d2062954121577cac2
Wocked61 <DylanP@csu.fullerton.edu>
2025-03-14 03:42:18 -0700

    project is finished from what I know will make changes according to the video right after

--- Homework2 - Student Files/my-project/script.js
+++ Homework2 - Student Files/my-project/script.js
@@ -6,9 +6,37 @@ const options = {
   }
 };
 let page = 1;
+let sortOption = 'SortBy';
+let searchQuery = '';
 
-function fetchMovies(page) {
-  fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, options)
+document.getElementById('sort').addEventListener('change', (event) => {
+  sortOption = event.target.value;
+  page = 1;
+  fetchMovies(page, sortOption, searchQuery);
+});
+
+document.getElementById('Movie_id').addEventListener('input', (event) => {
+  searchQuery = event.target.value;
+  page = 1;
+  fetchMovies(page, sortOption, searchQuery);
+});
+
+function fetchMovies(page, sortOption, searchQuery) {
+  let url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`;
+  
+  if (searchQuery) {
+    url = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&language=en-US&page=${page}`;
+  } else if (sortOption === 'ReleaseAsc') {
+    url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=release_date.asc`;
+  } else if (sortOption === 'ReleaseDesc') {
+    url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=release_date.desc`;
+  } else if (sortOption === 'RatingAsc') {
+    url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=vote_average.asc`;
+  } else if (sortOption === 'RatingDesc') {
+    url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=vote_average.desc`;
+  }
+
+  fetch(url, options)
     .then(res => res.json())
     .then(data => {
       console.log(data);
@@ -17,36 +45,31 @@ function fetchMovies(page) {
     .catch(err => console.error(err));
 }
 
-fetchMovies(page);
+fetchMovies(page, sortOption, searchQuery);
 
 const main = document.getElementById('main');
 const footer = document.getElementById('footer');
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 function showMovies(data) {
-
   main.innerHTML = '';
   footer.innerHTML = '';
 
   data.results.forEach(mov => {
-
-    
-    const{title,poster_path,release_date,vote_average} = mov;
+    const { title, poster_path, release_date, vote_average } = mov;
     const movEL = document.createElement('div');
     movEL.classList.add('movie');
     movEL.innerHTML = `
     <div class="box">
-      <img class="Picture" src="${poster_path?  IMG_URL+ poster_path: "https://play-lh.googleusercontent.com/xu-QRRdES_4ChdqrocEp7Dy4jtkST123_EX6wJ5pCq2VE7v4aSgt_Zw946h5DkN3iA"}" alt="${title}" id="Movie_Picture">
+      <img class="Picture" src="${poster_path ? IMG_URL + poster_path : "https://dennymfg.com/cdn/shop/products/ckgrayHigh_600x.jpg?v=1619109728"}" alt="${title}" id="Movie_Picture">
       <div>
           <h1 class="pictureText" id="Movie_Title">${title}</h1>
-          <p class="pictureDesc" id="Movie_Release">${"Release Date: "+release_date}</p>
-          <p class="pictureDesc" id="Movie_Rating">${"Rating: "+vote_average}</p>
+          <p class="pictureDesc" id="Movie_Release">${"Release Date: " + release_date}</p>
+          <p class="pictureDesc" id="Movie_Rating">${"Rating: " + vote_average}</p>
       </div>
     </div>`;
-  
-    main.appendChild(movEL);
-    //footer for buttons
 
+    main.appendChild(movEL);
   });
 
   const page = data.page;
@@ -57,7 +80,7 @@ function showMovies(data) {
       <button class="button" id="prev" onclick="prevPage()">Previous</button>
       <p id="page">${"Page " + page + " of " + total_pages}</p>
       <button class="button" id="next" onclick="nextPage()">Next</button>
-    </div>`
+    </div>`;
 
   footer.appendChild(page_number);
 }
@@ -66,13 +89,12 @@ function showMovies(data) {
 function prevPage() {
   if (page > 1) {
     page--;
-    fetchMovies(page);
+    fetchMovies(page, sortOption, searchQuery);
   }
 }
-
 
 //next button
 function nextPage() {
   page++;
-  fetchMovies(page);
+  fetchMovies(page, sortOption, searchQuery);
 }
